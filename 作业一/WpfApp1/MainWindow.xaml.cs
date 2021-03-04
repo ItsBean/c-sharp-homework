c5d1aae97e2e985c28d746a7234741b208d61070
wuxiefeng <965106195@qq.com>
2021-03-04 17:05:26 +0800

    updatehomework1

---
+++ 作业一/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public static bool isNumber(string str)
+        {
+            bool isMatch = Regex.IsMatch(str, @"^\d+$"); // 判断字符串是否为数字 的正则表达式
+            return isMatch;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string n1 = num1.Text;
+            string n2 = num2.Text;
+            if (isNumber(n1) && isNumber(n2))
+            {
+                int int1 = int.Parse(n1);
+                int int2 = int.Parse(n2);
+
+
+                int int3 = int1 + int2;
+                string ans = int3.ToString();
+                answer.Text = ans;
+            }
+            else
+                answer.Text = "字符错误!";
+        }
+
+        private void textbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void textbox_Copy1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string n1 = num1.Text;
+            string n2 = num2.Text;
+            int int1 = int.Parse(n1);
+            int int2 = int.Parse(n2);
+            int int3 = int1 * int2;
+            string ans = int3.ToString();
+            answer.Text = ans;
+        }
+
+        private void sub_Click(object sender, RoutedEventArgs e)
+        {
+            string n1 = num1.Text;
+            string n2 = num2.Text;
+            int int1 = int.Parse(n1);
+            int int2 = int.Parse(n2);
+            int int3 = int1 - int2;
+            string ans = int3.ToString();
+            answer.Text = ans;
+        }
+
+        private void div_Click(object sender, RoutedEventArgs e)
+        {
+            string ans;
+            double int3 = 0;
+            string n1 = num1.Text;
+            string n2 = num2.Text;
+            double int1 = double.Parse(n1);
+            double int2 = double.Parse(n2);
+            if (int2 == 0)
+                ans = "wrong!";
+            else
+            {
+                int3 = int1 / int2;
+                ans = int3.ToString();
+            }
+                answer.Text = ans;
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBoxItem_Selected(object sender, RoutedEventArgs e)
+        {   // int num;
+
+            // if (!int.TryParse(Text1.Text.Trim(), out num))
+            //{
+            //  MessageBox.Show("请输入数字");
+            //      }
+          
+            string n1 = num1.Text;
+            string n2 = num2.Text;
+            int int1, int2, int3 = 0;
+            if (isNumber(n1) && isNumber(n2))
+            {
+
+                int1 = int.Parse(n1);
+                int2 = int.Parse(n2);
+                int3 = int1 + int2;
+                string ans = int3.ToString();
+                answer.Text = ans;
+            }
+            else
+                answer.Text = "字符错误!";
+        }
+
+        private void ComboBoxItem_Selected_1(object sender, RoutedEventArgs e)
+        {
+            
+            string n1 = num1.Text;
+            string n2 = num2.Text;
+            int int1, int2, int3 = 0;
+
+            if (isNumber(n1) && isNumber(n2))
+            {
+                int1 = int.Parse(n1);
+                int2 = int.Parse(n2);
+                int3 = int1 - int2;
+                string ans = int3.ToString();
+                answer.Text = ans;
+            }
+            else
+                answer.Text = "字符错误!";
+        }
+
+        private void ComboBoxItem_Selected_2(object sender, RoutedEventArgs e)
+        {
+           
+            string n1 = num1.Text;
+            string n2 = num2.Text;
+            int int1, int2 = 0;
+            int int3;
+            if (isNumber(n1) && isNumber(n2))
+            {
+
+                int1 = int.Parse(n1);
+                int2 = int.Parse(n2);
+                int3 = int1 * int2;
+                string ans = int3.ToString();
+                answer.Text = ans;
+            }
+            else
+                answer.Text = "字符错误!";
+        }
+
+        private void ComboBoxItem_Selected_3(object sender, RoutedEventArgs e)
+        {
+            
+            string ans;
+            double int3 = 0;
+            string n1 = num1.Text;
+            string n2 = num2.Text;
+            double int1, int2 = 0;
+            if (isNumber(n1) && isNumber(n2))
+            {
+
+
+
+                int1 = double.Parse(n1);
+                int2 = double.Parse(n2);
+                if (int2 == 0)
+                    ans = "wrong!";
+                else
+                {
+                    int3 = int1 / int2;
+                    ans = int3.ToString();
+                }
+                answer.Text = ans;
+            }
+            else
+                answer.Text = "字符错误";
+        }
+    }
+}
+
+
+
---
+++ 作业一/homework-console/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace homework_console
+{
+    class Program
+    {   static bool isdigit(char arr) {
+            if (arr >= '0' && arr <= '9')
+                return true;
+            else
+                return false;
+
+        }
+        static int chartoint(char[] arr, int first, int last) {
+            int index = last - first;
+            int num = 0;
+            for (int i = 0; i <= index; i++) {
+                if (isdigit(arr[first + i]))
+                {   
+                    
+                    num += ((int)(arr[first + i]) - (int)('0')) * (int)Math.Pow(10,index - i);
+                }
+                else
+                {
+                    Console.WriteLine("wrong!");
+                    Environment.Exit(0);
+                }
+               
+
+            }
+           // Console.WriteLine("text num is :{0}", num);
+            return num;
+        }
+        static int findcal(char[] arr,int length) {
+            for (int i = 0; i < length; i++) {
+                if (arr[i] == '+' || arr[i] == '-' || arr[i] == '*' || arr[i] == '/') {
+                    return i;
+                }
+                
+            }
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            string num;
+            Console.WriteLine("\t\t\t**this is a simple calculator**");
+            Console.Write("please input equation:");
+            num = Console.ReadLine();
+            char[] getinput = num.ToCharArray();
+            int length = getinput.Length;
+            //num转char数组 getinput
+            int cal = findcal(getinput,length);
+            if (cal == -1) {
+                Console.WriteLine("wrong！");
+                Environment.Exit(0);
+
+            }
+            int num1 = chartoint(getinput, 0, cal - 1);
+            int num2 = chartoint(getinput, cal + 1, length - 1);
+            int num3 = 0;
+            double num4 = 0;
+            switch (getinput[cal])
+            {
+                case '+':
+                    num3 = num1 + num2;
+                    break;
+                case '-':
+                    num3 = num1 - num2;
+                    break;
+                case '*': 
+                    num3 = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 == 0)
+                    {
+                        Console.WriteLine("wrong!");
+                        Environment.Exit(0);
+                    }
+                    num4 = (double)num1 / (double)num2;
+                    break;
+                default:
+                    Console.WriteLine("wrong!");
+                    Environment.Exit(0);
+                    break;
+            }
+            if (getinput[cal] != '/')
+                Console.WriteLine("answer is {0}", num3);
+            else
+                Console.WriteLine("answer is {0}",num4);
+            
+ 
+            
+        }
+    }
+}
